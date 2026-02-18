845052ef790d4297a4532f5cd07cc0c9670df102
Ahmad <ahmad@Ahmads-MacBook.local>
2026-02-18 11:03:48 +0530

    Hosted static files

--- app.py
+++ app.py
@@ -3,7 +3,8 @@ import sqlite3
 from werkzeug.security import generate_password_hash, check_password_hash
 from functools import wraps
 
-app = Flask(__name__)
+app = Flask(__name__, template_folder="templates", static_folder="static")
+
 app.secret_key = 'your_secret_key_here'
 
 
